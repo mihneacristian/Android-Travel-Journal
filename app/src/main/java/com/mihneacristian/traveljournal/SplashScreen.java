7aee7bf3813f9320b35d0f753e232e3b7bc56aa8
mihneacristianpopa <mihnea-cristian.popa@outlook.com>
2020-06-28 13:38:03 +0300

    Commit

--- app/src/main/java/com/mihneacristian/traveljournal/SplashScreen.java
+++ app/src/main/java/com/mihneacristian/traveljournal/SplashScreen.java
@@ -21,6 +21,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },1000);
+        },1200);
     }
 }
