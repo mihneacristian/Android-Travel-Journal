6cbc26a8271a8b3899dd328c369b775e84c4440d
mihneacristianpopa <mihnea-cristian.popa@outlook.com>
2020-07-01 18:09:09 +0300

    Commit

--- app/src/main/java/com/mihneacristian/traveljournal/SplashScreen.java
+++ app/src/main/java/com/mihneacristian/traveljournal/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LogInActivity.class);
                 startActivity(intent);
                 finish();
             }
---
+++ app/src/main/java/com/mihneacristian/traveljournal/ui/home/AboutUsViewModel.java
@@ -0,0 +1,19 @@
+package com.mihneacristian.traveljournal.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AboutUsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AboutUsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
