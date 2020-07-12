1f194f02b3335545d68bad283c784f8bda9aa755
mihneacristianpopa <mihnea-cristian.popa@outlook.com>
2020-07-12 13:22:24 +0300

    Commit

--- app/src/main/java/com/mihneacristian/traveljournal/Activities/NewTripActivity.java
+++ app/src/main/java/com/mihneacristian/traveljournal/Activities/NewTripActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.app.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
@@ -20,6 +21,7 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -42,44 +44,49 @@ import com.mihneacristian.traveljournal.R;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
 public class NewTripActivity extends AppCompatActivity {
 
-    ImageView returnToHomeImageView;
-    ImageView openCameraImageView;
-    ImageView openGalleryImageView;
-    ImageView uploadedPhotoImageView;
+    private ImageView returnToHomeImageView;
+    private ImageView openCameraImageView;
+    private ImageView openGalleryImageView;
+    private ImageView uploadedPhotoImageView;
 
-    String pathToFile;
-    String photoURL;
+    private String pathToFile;
+    private String photoURL;
 
-    StorageReference storageReference;
-    DatabaseReference databaseTripReference;
+    private StorageReference storageReference;
+    private DatabaseReference databaseTripReference;
 
     public static final int CAMERA_REQUEST_CODE = 101;
     public static final int GALLERY_REQUEST_CODE = 102;
 
-    EditText tripNameEditText;
-    EditText destinationEditText;
+    private EditText tripNameEditText;
+    private EditText destinationEditText;
 
-    RadioGroup tripTypeRadioGroup;
-    RadioButton radioButtonCityBreak;
-    RadioButton radioButtonSeaSide;
-    RadioButton radioButtonMountain;
+    private RadioGroup tripTypeRadioGroup;
+    private RadioButton radioButtonCityBreak;
+    private RadioButton radioButtonSeaSide;
+    private RadioButton radioButtonMountain;
 
-    TextView showPriceEUR;
-    TextView startDateTextView;
-    TextView endDateTextView;
+    private TextView showPriceEUR;
+    private TextView startDateTextView;
+    private TextView endDateTextView;
 
-    SeekBar priceSeekBar;
+    private SeekBar priceSeekBar;
 
-    Button startDateButton;
-    Button endDateButton;
-    Button addTripButton;
+    private Button startDateButton;
+    private Button endDateButton;
+    private Button addTripButton;
 
-    RatingBar ratingBarTrip;
+    private RatingBar ratingBarTrip;
+
+    private int year;
+    private int month;
+    private int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -261,5 +268,37 @@ public class NewTripActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Please enter Trip Name", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void onClickPickStartDate(View view) {
+        final Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        calendar.getTime();
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                startDateTextView.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    public void onClickPickEndDate(View view) {
+        final Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        calendar.getTime();
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                endDateTextView.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
     }
 }
